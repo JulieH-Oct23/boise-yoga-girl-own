9b26065e000a1613e473143259b8ae300e4bd7f6
Julie Hart <147504798+JulieH-Oct23@users.noreply.github.com>
2025-06-28 15:03:32 -0600

    photofile name

---
+++ src/images/index.js
@@ -0,0 +1,133 @@
+import BackwardBending from "./BackwardBending.png";
+import Boat from "./Boat.png";
+import BoiseYogaGirl from "./BoiseYogaGirl.png";
+import Bridge from "./Bridge.png";
+import Camel from "./Camel.png";
+import Cat from "./Cat.png";
+import ClosedChild from "./ClosedChild.png";
+import Cobra from "./Cobra.png";
+import Cow from "./Cow.png";
+import CowFace from "./CowFace.png";
+import CrescentLunge from "./CrescentLunge.png";
+import CrescentMoon from "./CrescentMoon.png";
+import Crow from "./Crow.png";
+import Dancer from "./Dancer.png";
+import DownwardDog from "./DownwardDog.png";
+import Eagle from "./Eagle.png";
+import Easy from "./Easy.png";
+import ExtendedSideAngle from "./ExtendedSideAngle.png";
+import Fish from "./Fish.png";
+import FloorBow from "./FloorBow.png";
+import ForwardFold from "./ForwardFold.png";
+import FourLimbbedStaff from "./Four-LimbbedStaff.png";
+import Garland from "./Garland.png";
+import Goddess from "./Goddess.png";
+import HalfMoon from "./HalfMoon.png";
+import HeartMelting from "./HeartMelting.png";
+import Hero from "./Hero.png";
+import HighPlank from "./HighPlank.png";
+import LegUpWall from "./LegUpWall.png";
+import Locust from "./Locust.png";
+import LowLunge from "./LowLunge.png";
+import LowPlank from "./LowPlank.png";
+import Mermaid from "./Mermaid.png";
+import Mountain from "./Mountain.png";
+import Pigeon from "./Pigeon.png";
+import Puppy from "./Puppy.png";
+import ReverseTableTop from "./ReverseTableTop.png";
+import RevolvedSeatedHeadToKnee from "./RevolvedSeatedHeadToKnee.png";
+import RevolvedTriangle from "./RevolvedTriangle.png";
+import Savasana from "./Savasana.png";
+import SeatedForeheadToKnee from "./SeatedForeheadToKnee.png";
+import SeatedForwardFold from "./SeatedForwardFold.png";
+import SeatedTwist from "./SeatedTwist.png";
+import ShoulderStand from "./ShoulderStand.png";
+import SideChair from "./SideChair.png";
+import SideEagle from "./SideEagle.png";
+import SideGarland from "./SideGarland.png";
+import SidePlank from "./SidePlank.png";
+import SideStanding from "./SideStanding.png";
+import Snail from "./Snail.png";
+import StandingHandToe from "./StandingHandToe.png";
+import StandingSeparationForwardFold from "./StandingSepa...wardFold.png";
+import StandingSideStretch from "./StandingSideStretch.png";
+import StandingSplit from "./StandingSplit.png";
+import ThreeLegDownDog from "./ThreeLegDownDog.png";
+import ToeStand from "./ToeStand.png";
+import TopSeatedForeheadToKnee from "./TopSeatedFor...dToKnee.png";
+import Tree from "./Tree.png";
+import Triangle from "./Triangle.png";
+import UpwardFacingDog from "./UpwardFacingDog.png";
+import Warrior1 from "./Warrior1.png";
+import Warrior2 from "./Warrior2.png";
+import Warrior3 from "./Warrior3.png";
+import Wheel from "./Wheel.png";
+import WingedDragon from "./WingedDragon.png";
+
+export default {
+  BackwardBending,
+  Boat,
+  BoiseYogaGirl,
+  Bridge,
+  Camel,
+  Cat,
+  ClosedChild,
+  Cobra,
+  Cow,
+  CowFace,
+  CrescentLunge,
+  CrescentMoon,
+  Crow,
+  Dancer,
+  DownwardDog,
+  Eagle,
+  Easy,
+  ExtendedSideAngle,
+  Fish,
+  FloorBow,
+  ForwardFold,
+  FourLimbbedStaff,
+  Garland,
+  Goddess,
+  HalfMoon,
+  HeartMelting,
+  Hero,
+  HighPlank,
+  LegUpWall,
+  Locust,
+  LowLunge,
+  LowPlank,
+  Mermaid,
+  Mountain,
+  Pigeon,
+  Puppy,
+  ReverseTableTop,
+  RevolvedSeatedHeadToKnee,
+  RevolvedTriangle,
+  Savasana,
+  SeatedForeheadToKnee,
+  SeatedForwardFold,
+  SeatedTwist,
+  ShoulderStand,
+  SideChair,
+  SideEagle,
+  SideGarland,
+  SidePlank,
+  SideStanding,
+  Snail,
+  StandingHandToe,
+  StandingSeparationForwardFold,
+  StandingSideStretch,
+  StandingSplit,
+  ThreeLegDownDog,
+  ToeStand,
+  TopSeatedForeheadToKnee,
+  Tree,
+  Triangle,
+  UpwardFacingDog,
+  Warrior1,
+  Warrior2,
+  Warrior3,
+  Wheel,
+  WingedDragon,
+};
