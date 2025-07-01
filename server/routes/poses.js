284516bf2d7219e383e42c61e4d56883fae2fa0d
Julie Hart <147504798+JulieH-Oct23@users.noreply.github.com>
2025-07-01 12:34:46 -0600

    restart

--- server/routes/poses.js
+++ server/routes/poses.js
@@ -1,30 +1,3 @@
-// import express from "express";
-// import Pose from "../models/PoseModel.js";
-
-// const router = express.Router();
-
-// router.get("/", async (req, res) => {
-//   try {
-//     const poses = await Pose.find();
-//     res.json(poses);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
-
-// router.post("/", async (req, res) => {
-//   const { name, image, category, description } = req.body;
-//   const newPose = new Pose({ name, image, category, description });
-
-//   try {
-//     const savedPose = await newPose.save();
-//     res.status(201).json(savedPose);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
-
-// export default router;
 import express from "express";
 import Pose from "../models/PoseModel.js";
 
