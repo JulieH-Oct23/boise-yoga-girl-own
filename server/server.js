3711515bef25e3fa233ccd04f3f67589e6c4547c
Julie Hart <147504798+JulieH-Oct23@users.noreply.github.com>
2025-07-16 10:39:00 -0600

    server running mesage

--- server/server.js
+++ server/server.js
@@ -44,3 +44,7 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Boise Yoga Girl backend is running 🧘‍♀️");
+});
